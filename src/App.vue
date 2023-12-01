e6dec50b5ba228b4b3b7ede78bafcdbd78f5379d
Artem <146167306+Artem38programmer@users.noreply.github.com>
2023-12-01 10:05:51 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <component :is='layout'>
+      <router-view></router-view>
+    </component>
+  </div>
+  
+</template>
+<script>
+import EmptyLayout from './layout/EmptyLayout';
+import MainLayout from './layout/MainLayout'
+
+export default {
+   metaInfo: {
+      // title will be injected into parent titleTemplate
+      title: 'About Us'
+    },
+  components : {
+    EmptyLayout, MainLayout
+  },
+  computed: {
+    layout() {
+      console.log(this.$route.meta);
+      return (this.$route.meta.layout || 'empty') + '-layout'
+    }
+  },
+
+  
+}
+</script>
+
+<style>
+@import '~materialize-css/dist/css/materialize.min.css';
+@import './assets/index.css';
+
+</style>
